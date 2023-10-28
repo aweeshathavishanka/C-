3af01e713bccaf93e1e9514194fc0305b95698b8
aweeshathavishanka <Your GitHub Email>
2023-10-28 10:37:52 +0530

    first comment

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,12 @@
-// Your First C++ Program
-
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!";
+int main() 
+{    
+    cout << "Size of char: " << sizeof(char) << " byte" << endl;
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+
     return 0;
 }
